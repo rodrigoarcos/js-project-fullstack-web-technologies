a11087e48d2d7db99c8fab87a5f84183241955c5
Rodrigo Arcos <44504239+rodrigoarcos@users.noreply.github.com>
2021-04-11 22:00:34 +0200

    players addition and init of hand dealing. Not yet implemented.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-let deck = require("./model/model.json");
+let deck = require("./model/cards.json");
+let players = require("./model/players.json");
 
 class Card {
   constructor(suit, value) {
@@ -10,12 +11,30 @@ class Card {
   }
 }
 
-function shuffle(wholeDeck) {
+class Player {
+  constructor(name, surname, hand, points, turn) {
+    this.name = name;
+    this.surname = surname;
+    this.hand = hand;
+    this.points = points;
+    this.turn = turn;
+  }
+}
+
+let shuffle = (wholeDeck) => {
   for (let i = wholeDeck.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [wholeDeck[i], wholeDeck[j]] = [wholeDeck[j], wholeDeck[i]];
   }
   return wholeDeck;
-}
+};
 
 console.log(shuffle(deck));
+
+let handGenerator = (wholeDeck, players, round) => {
+    for(let i = 0; i <= round; i++) {
+        for(let player in players){
+            player.hand.push(wholeDeck[i]);
+        }
+    }
+}
