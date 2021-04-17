4df73dba9b58af0ab4bc699e08855b3e12079a77
Rodrigo Arcos <44504239+rodrigoarcos@users.noreply.github.com>
2021-04-17 20:01:45 +0200

    dealing hands algorithm. Completed

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 let deck = require("./model/cards.json");
-let players = require("./model/players.json");
+let participants = require("./model/players.json");
 
 class Card {
   constructor(suit, value) {
     this.suit = suit;
     this.value = value;
-  }
-  get value() {
-    return this.value();
   }
 }
 
@@ -21,6 +18,24 @@ class Player {
   }
 }
 
+let cards = [];
+for (let card in deck) {
+  let auxCard = new Card(deck[card].suit, deck[card].value);
+  cards.push(auxCard);
+}
+
+let players = [];
+for (let player in participants) {
+  let auxPlayer = new Player(
+    participants[player].name,
+    participants[player].surname,
+    participants[player].hand,
+    participants[player].points,
+    participants[player].turn
+  );
+  players.push(auxPlayer);
+}
+
 let shuffle = (wholeDeck) => {
   for (let i = wholeDeck.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -29,12 +44,15 @@ let shuffle = (wholeDeck) => {
   return wholeDeck;
 };
 
-console.log(shuffle(deck));
+console.log(shuffle(cards));
 
 let handGenerator = (wholeDeck, players, round) => {
-    for(let i = 0; i <= round; i++) {
-        for(let player in players){
-            player.hand.push(wholeDeck[i]);
-        }
+  for (let i = 0; i <= round; i++) {
+    for (let player in players) {
+      players[player].hand.push(wholeDeck[i]);
+      wholeDeck.splice(i, 1);
     }
-}
+  }
+};
+
+console.log(handGenerator(shuffle(deck), players, 2));
